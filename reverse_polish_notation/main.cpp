2159c42af8e0e640a1bfcd6c552ae69fe4e2720d
Jakub Baron <jakubaron@gmail.com>
2019-01-31 23:08:55 +0000

    Defying inputs and operators

--- reverse_polish_notation/main.cpp
+++ reverse_polish_notation/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#include <stack>
 //
 
 int main(int argc, char** argv) {
-
+  std::string input = "2+3+4*5";
+  static const std::string operators = "+-/*";
   return EXIT_SUCCESS;
 }
