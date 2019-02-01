c0ede70cbe9dc66499bf9f8f96d3a53aad91f5d5
Jakub Baron <jakubaron@gmail.com>
2019-02-01 22:47:05 +0000

    More fake commits

--- reverse_polish_notation/main.cpp
+++ reverse_polish_notation/main.cpp
@@ -2,6 +2,11 @@
 #include <stack>
 //
 
+auto calculate(const std::string& input) noexcept -> double {
+  double result = 0.0;
+  return result;
+}
+
 int main(int argc, char** argv) {
   std::string input = "2+3+4*5";
   static const std::string operators = "+-/*";
