b8736bdece2bab57caa682e5bfb5de3d2643c553
Jakub Baron <jakubaron@gmail.com>
2019-02-02 20:41:35 +0000

    Reverse polish notation for digits

--- reverse_polish_notation/main.cpp
+++ reverse_polish_notation/main.cpp
@@ -1,17 +1,54 @@
 #include <iostream>
 #include <stack>
-//
 
 auto calculate(const std::string& input) noexcept -> double {
+  static const std::string operators = "+-/*";
   double result = 0.0;
+  if(input.empty()) {
+    return result;
+  }
+  std::stack<double> calc;
   for(const auto& c: input) {
-    
+    if(operators.find(std::string(1, c)) == std::string::npos) {
+      calc.emplace(static_cast<double>(c - '0'));
+      continue;
+    }
+    if(calc.empty()) {
+      return result; 
+    }
+    double a = calc.top();
+    calc.pop();
+    double b = a;
+    if(!calc.empty()) {
+      b = calc.top(); 
+      calc.pop();
+    }
+    switch(c) {
+      case '+':
+        calc.emplace(a+b);
+        break;
+      case '-':
+        calc.emplace(a-b);
+        break;
+      case '/':
+        calc.emplace(a/b);
+        break;
+      case '*':
+        calc.emplace(a*b);
+        break;
+    }
+  }
+  result = calc.top();
+  calc.pop();
+  if(!calc.empty()) { 
+    std::cerr << "Stack wasn't empty at the end! :o" << std::endl;
   }
   return result;
 }
 
 int main(int argc, char** argv) {
-  std::string input = "2+3+4*5";
-  static const std::string operators = "+-/*";
+  std::string input = "21+3**";
+  const auto result = calculate(input);
+  std::cout << result << std::endl;
   return EXIT_SUCCESS;
 }
